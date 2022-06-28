2f0ac7f69c07efeb321d8d29a98136d0fb69285c
unknown <sss-hhh-813@hotmail.com>
2022-06-28 09:49:26 +0300

    im card

--- lib/main.dart
+++ lib/main.dart
@@ -3,88 +3,79 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
- class MyApp extends StatelessWidget {
 
-   Widget build(BuildContext context){
-return MaterialApp(
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children:<Widget> [
+            children: <Widget>[
               CircleAvatar(
                 radius: 50,
-                backgroundImage:AssetImage('images/angela.png'),
+                backgroundImage: AssetImage('images/angela.png'),
               ),
-            Text('Angela Yu' ,
-            style: TextStyle(
-              fontSize: 20,
-              fontFamily: 'Pacifico',
-              fontWeight: FontWeight.bold,
-              color: Colors.white
+              Text(
+                'Angela Yu',
+                style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'Pacifico',
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
               ),
-              ),
-              Text('FLUTTER DEVELOPER' ,
-            style: TextStyle(
-              fontSize: 10,
-              fontFamily: 'Sourxe Sans pro',
-              letterSpacing: 2.5,
-              fontWeight: FontWeight.bold,
-              color: Colors.teal.shade100,
-
-              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontSize: 10,
+                  fontFamily: 'Sourxe Sans pro',
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal.shade100,
+                ),
               ),
               SizedBox(
-                height: 20.0,width: 150.0,
-                child: Divider(color:Colors.teal.shade100 
-                ),
+                height: 20.0,
+                width: 150.0,
+                child: Divider(color: Colors.teal.shade100),
               ),
-              
-            Card(
-              margin: EdgeInsets.symmetric(vertical: 10.0,
-              horizontal: 25.0),
-              child: ListTile(
-                leading:Icon(Icons.phone,
-                color: Colors.teal,),
-                title:Text('+44 123 456 789',
-                style: TextStyle(
-                  color: Colors.teal.shade900,
-                  fontFamily: 'Source Sans pro',
-                  fontSize: 20.0
-                ),
-                ),
-              ),
-            ),
-            
-
-          
-            Card(
-             margin: EdgeInsets.symmetric(
-              vertical: 10.0,horizontal: 25.0
-              ),
-              
-              child: ListTile(
-               
-                leading:Icon(Icons.email,
-                color:Colors.teal ,
-                ),
-               
-                 title:Text(
-                  'angel@emil.com',
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.teal.shade900,
-                    fontFamily: 'Source Sans pro'
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
                   ),
-                 ),
+                  title: Text(
+                    '+44 123 456 789',
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans pro',
+                        fontSize: 20.0),
+                  ),
+                ),
               ),
-            )
-            
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'angel@emil.com',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans pro'),
+                  ),
+                ),
+              )
             ],
           ),
         ),
       ),
     );
-   }
- } 
+  }
+}
